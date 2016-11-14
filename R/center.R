1b264dd6dba4fc070a9071ed58795a690b095704
Bruno Vidigal <bruno.caetanovidigal@fao.org>
2016-11-14 15:30:49 +0100

    Added description fot the function center

--- R/center.R
+++ R/center.R
@@ -1,3 +1,15 @@
+##' Center
+##' These four different functions provide estimates for the food consumption 
+##' values in year t+1 given the consumption in year t, changes in income, and 
+##' the elasticity of the particular commodity.
+##' 
+##' @param type A character value.
+##' 
+##' ##' @export
+##' 
+
+
+
 
 center <- function(type = c("0", "1", "2", "3", "NA"), ...) {
     
